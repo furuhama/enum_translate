3da1faf60bdfddf352e1535c35254c1acf8ffdd0
furuhama <furuhama@users.noreply.github.com>
2018-12-09 15:21:41 +0900

    Improve comments in script

--- lib/enum_translate/enum_translate_concern.rb
+++ lib/enum_translate/enum_translate_concern.rb
@@ -8,8 +8,8 @@ module EnumTranslate
   def human_attribute_text(attribute_name)
     attribute_value = public_send(attribute_name)
 
-    # enum の attribute を取得した場合、 String 型の値が返ってくるはず
-    # そうでない場合には、引数の組み合わせはおかしいことが考えられるので例外を投げる
+    # `attribute_value` is a return value of calling ActiveRecord::Enum attribute instance API.
+    #  It should be a String value, and raise an error if not.
     raise EnumTranslateArgumentError, "#{self.class.name.underscore}.#{attribute_name} should be String value." unless attribute_value.is_a?(String)
 
     I18n.t "activerecord.attributes.#{self.class.name.underscore}/#{attribute_name}.#{attribute_value}", default: attribute_value
@@ -19,8 +19,8 @@ module EnumTranslate
     def human_attribute_options(attribute_name)
       attribute_values = public_send(attribute_name.to_s.pluralize)
 
-      # enum の attributes を取得した場合、 ActiveSupport::HashWithIndifferentAccess が返ってくるはず
-      # そのため Hash 型の値が返ってきていない場合、引数の組み合わせがおかしいことが考えられるので例外を投げる
+      # `attribute_values` is a return value of calling ActiveRecord::Enum attribute class API.
+      #  It should be a ActiveSupport::HashWithIndifferentAccess value, and raise an error if it is not inherited a Hash class.
       raise EnumTranslateArgumentError, "#{name}.#{attribute_name} should be Hash-type value." unless attribute_values.is_a?(Hash)
 
       option_values_hash = I18n.t "activerecord.attributes.#{name.underscore}/#{attribute_name}", default: attribute_values

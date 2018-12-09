15932c4dd3118f29ff7e35eb686a70b9332a3d77
furuhama <furuhama@users.noreply.github.com>
2018-12-09 16:13:19 +0900

    Add test for normal cases

--- spec/enum_translate_spec.rb
+++ spec/enum_translate_spec.rb
@@ -1,9 +1,35 @@
 RSpec.describe EnumTranslate do
-  it "has a version number" do
-    expect(EnumTranslate::VERSION).not_to be nil
+  class Article < ActiveRecord::Base
+    include EnumTranslate
+
+    enum category: { finance: 0, lifehack: 1, technology: 2 }
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.human_attribute_options' do
+    subject { Article.human_attribute_options(:category) }
+
+    let(:result) do
+      [
+        ["経済", "finance"],
+        ["生活豆知識", "lifehack"],
+        ["技術", "technology"]
+      ]
+    end
+
+    it { is_expected.to eq result }
+  end
+
+  describe '#human_attribute_text' do
+    subject { article.human_attribute_text(:category) }
+
+    let(:article) { Article.new(category: :finance) }
+
+    it { is_expected.to eq '経済' }
+  end
+
+  describe 'gemspec' do
+    it 'has a version number' do
+      expect(EnumTranslate::VERSION).not_to be nil
+    end
   end
 end
